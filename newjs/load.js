0b227444d6cdb90c1e44974ce46ba0923efca65b
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-05-28 14:41:11 +0200

    Chore: removed comments

--- newjs/load.js
+++ newjs/load.js
@@ -1,29 +1,25 @@
 class Countdown {
   constructor() {
-    this.count = 10; // Countdown time in seconds
-    // Start the countdown interval when the Countdown object is created
+    this.count = 10;
     this.countdownInterval = setInterval(() => {
-      this.tick(); // Call tick method every second
-    }, 1000); // 1000 milliseconds = 1 second
+      this.tick();
+    }, 1000);
   }
 
   tick() {
-    this.count--; // Decrement countdown every second
-    console.log(this.count); // Log the countdown value for debugging
-    // If countdown reaches 0, redirect to the specified HTML file and stop the countdown
+    this.count--;
+    console.log(this.count);
     if (this.count === 0) {
-      this.stop(); // Call stop method to clear the interval
-      // Redirect to the specified HTML file after 10 seconds
+      this.stop();
       window.location.href = "html/signup.html";
     }
   }
 
   stop() {
-    clearInterval(this.countdownInterval); // Clear the countdown interval
+    clearInterval(this.countdownInterval);
   }
 }
 
-// Create a new instance of the Countdown class when the page is loaded
 window.onload = function () {
   new Countdown();
 };
